1a2f798743be2e0f5c5cbaf1092f05d3d2bcaa84
Matthias Müller <matthias.mueller@trivadis.com>
2019-07-04 15:17:56 +0200

    WIP

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios'
+import { HelloWorld } from './components/hello-world';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      message: '',
+      dataFromParent: 'Hello from parent!'
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleSubButtonClicked() {
+    debugger;
+    this.setState({ message: 'Hello from child' });
+  }
+
+  handleClick() {
+    axios.get('https://api.github.com/users/DrMueller')
+      .then(response => {
+        this.setState({ message: response.data.login });
+      })
+      .catch(err => {
+        debugger;
+      });
+  }
+
+  render() {
+    return (
+      <div className='button__container'>
+        <button className='button' onClick={this.handleClick}>Click Me</button>
+        <p>Message: {this.state.message}</p>
+        <HelloWorld dataFromParent={this.state.dataFromParent} onButtonClicked={this.handleSubButtonClicked.bind(this)} />
+      </div>
+    )
+  }
 }
 
 export default App;
